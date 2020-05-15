32c8b2361588728f2bccab8a3990d7c23f6bd7dd
Jack <29169102+Jack5079@users.noreply.github.com>
2020-05-15 13:19:40 -0400

    remove an else if so i am not cringe

--- ping.js
+++ ping.js
@@ -32,9 +32,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // The following lines allow the ping function to be loaded via commonjs, AMD,
 // and script tags, directly into window globals.
 // Thanks to https://github.com/umdjs/umd/blob/master/templates/returnExports.js
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) { define([], factory); } else if (typeof module === 'object' && module.exports) { module.exports = factory(); } else { root.ping = factory(); }
-}(this, function () {
+(()=>{
 
     /**
      * Creates and loads an image element by url.
@@ -71,6 +69,5 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
             setTimeout(function () { reject(Error('Timeout')); }, 5000);
         });
     }
-
-    return ping;
-}));
+  window.ping = ping
+  })()
